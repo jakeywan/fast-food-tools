782fcd3c56f8cb510d3651de2fa819e2bdcf9858
Jake Allen <jake@hintsy.co>
2021-10-28 16:11:46 -0700

    Ability to encode single image or many images.

---
+++ src/rleEncodeSingleImage.js
@@ -0,0 +1,21 @@
+import { PNGCollectionEncoder } from '@nouns/sdk';
+import { readPngFile } from 'node-libpng';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const DESTINATION = path.join(__dirname, '../output/singleWearable.json');
+
+const encode = async () => {
+  const encoder = new PNGCollectionEncoder()
+  const filepath = path.join(__dirname, '../wearablesImages/customBodies/stain.png')
+  const image = await readPngFile(filepath)
+  encoder.encodeImage(filepath.replace(/\.png$/, ''), image)
+  await encoder.writeToFile(DESTINATION)
+  
+}
+
+encode()
